cee2d5e375581b9e814452157df8cea8ebe433bf
henryssantos <henry.reimao@ba.estudante.senai.br>
2026-04-07 20:34:56 -0300

    Update Main.java

--- Sistema Bancario/src/Main.java
+++ Sistema Bancario/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         ContaCorrente cc1 = new ContaCorrente("Henry Santos Reimão",2000,5000);
         ContaCorrente cc2 = new ContaCorrente("Pedro Canuto", 50,100);
+        ContaCorrente cc3 = new ContaCorrente("Jesiane Silva",20,200);
 
         ContaPoupanca cp1 = new ContaPoupanca("Jhonatan",2000,0.05);
         ContaPoupanca cp2 = new ContaPoupanca("Luan",800,0.04);
@@ -15,6 +16,9 @@ public class Main {
         System.out.println("\n=== Conta Corrente 2 ===");
         cc2.exibirInfo();
         System.out.println("Saldo após saque: "+cc2.sacar(1200));
+
+        cc3.exibirInfo();
+        System.out.println("Saldo após saque:"+cc3.sacar(10));
 
         System.out.println("\n=== Conta Poupança 1 ===");
         cp1.exibirInfo();
